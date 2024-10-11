8c516c93ceeb3cdfc09f26f6d13e5938bb7f507f
anh tuan <nguyenanhtuan050302@gmail.com>
2024-10-11 17:48:19 +0700

    reviews and report

--- GreenCarPark/carpark/greencarpark/urls.py
+++ GreenCarPark/carpark/greencarpark/urls.py
@@ -16,6 +16,7 @@ router.register('subscription-type', SubscriptionTypeViewSet)
 router.register('parking-history', ParkingHistoryViewSet)
 router.register('reviews', ReviewsViewSet)
 router.register('complaint', ComplaintViewSet)
+router.register('payment', PaymentViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', car_park_admin_site.urls),
